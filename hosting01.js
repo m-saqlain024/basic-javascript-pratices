c50547d9ad2a2535497bd5a08e94c9a954f0637a
BeingMurshid <murshid717104@gmail.com>
2022-10-17 10:14:13 +0500

    hosting in js

---
+++ hosting01.js
@@ -0,0 +1,20 @@
+document.getElementById('demo1').innerHTML= " hosting pratices"; 
+
+
+
+// function hosting 
+
+ 
+ let a = saqi(12,32);    // function call
+
+function saqi(a ,b){    // function declaration
+    return a+b
+}
+document.getElementById('demo2').innerHTML= a; 
+
+
+// variable hosting 
+document.getElementById('demo3').innerHTML= b; 
+
+
+let b="this is alredy display output "
